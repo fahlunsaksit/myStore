35e370121dad29ef883a0e7644f2417b7e2d4a6a
fahlunsaksit <saksit.ban@gmail.com>
2019-09-03 21:39:24 +0700

    test

---
+++ src/app/products.ts
@@ -0,0 +1,14 @@
+export const products = [
+    {
+        name: 'AAA',
+        price : 123
+    },
+    {
+        name: 'BBB',
+        price : 123
+    },
+    {
+        name: 'CCC',
+        price : 123
+    },
+]
